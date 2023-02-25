1cbc9dd6a56d69518d6825215ec538934c8098e1
Timofey Krutov <alterator76@yandex.ru>
2023-02-25 22:31:20 +0300

    first commit

---
+++ server.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"log"
+)
+
+func sayhello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Привет!")
+}
+
+func main() {
+	http.HandleFunc("/", sayhello) // Устанавливаем роутер
+	err := http.ListenAndServe(":8080", nil) // устанавливаем порт веб-сервера
+
+	// Если хотите использовать https, то вместо ListenAndServe используйте ListenAndServeTLS
+	// err := http.ListenAndServeTLS(":8080", "cert.pem", "key.pem", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
